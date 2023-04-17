0a97f8ad75a900b243d50d19caa47d32f53d9100
KaanDoesNothing <kaanyt123@gmail.com>
2023-04-17 14:05:51 +0200

    fix embed

--- server/api/file/info/[file].ts
+++ server/api/file/info/[file].ts
@@ -2,13 +2,15 @@ import {storageClient} from "~/server/storage";
 import {DB_File} from "~/server/db";
 
 export default defineEventHandler(async (e) => {
+    const config = useRuntimeConfig();
+
     if(e.req.method === "GET") {
         const file_name = e.context?.params?.file;
         const file = await DB_File.findOne({file_name});
 
         if(!file) return {error: {message: "File doesn't exist"}}
 
-       return {data: {file, url: `https://${e.req.headers.host}/file/${file_name}`}};
+       return {data: {file, url: `${config.BASE}/file/${file_name}`}};
     }
 });
 
