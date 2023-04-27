c328dc8459e6cddb72b1def4a6164618d4ee67ff
KaanDoesNothing <kaanyt123@gmail.com>
2023-04-27 18:53:44 +0200

    Fix file fetching

--- server/routes/file/[file].ts
+++ server/routes/file/[file].ts
@@ -2,7 +2,8 @@ import {Attachment, DB_File} from "~/server/db";
 
 export default defineEventHandler(async (e) => {
     if(e.req.method === "GET") {
-        const file_name = e.context?.params?.file;
+        const file_name_raw = e.context?.params?.file;
+        const file_name = new URLSearchParams(`name=${file_name_raw}`).get("name");
         const file = await DB_File.findOne({file_name});
 
         if(!file) return {error: {message: "File doesn't exist"}}
